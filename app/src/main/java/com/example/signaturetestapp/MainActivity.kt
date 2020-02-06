9adc47de74d550188817ef5ee2809c6e369609dc
Chris Raley <craley@doubledutch.me>
2020-02-06 11:40:08 -0700

    Initial checkin

---
+++ app/src/main/java/com/example/signaturetestapp/MainActivity.kt
@@ -0,0 +1,159 @@
+package com.example.signaturetestapp
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.signaturetestapp.R.id
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.security.KeyFactory
+import java.security.Signature
+import java.security.spec.X509EncodedKeySpec
+import java.util.Base64
+import java.util.regex.Pattern
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        fun loadPEMFile(inputStream: InputStream): ByteArray {
+            val pubBytes = toByteArray(inputStream)
+            val pem = String(pubBytes)
+            val parse = Pattern.compile("(?m)(?s)^---*BEGIN.*---*$(.*)^---*END.*---*$.*")
+            val encoded = parse.matcher(pem).replaceFirst("$1")
+            return Base64.getMimeDecoder().decode(encoded)
+        }
+
+        fun toByteArray(inputStream: InputStream): ByteArray {
+            val outputStream = ByteArrayOutputStream()
+            val buffer = ByteArray(1024)
+
+            var len: Int
+            do {
+                len = inputStream.read(buffer)
+                if (len != -1) {
+                    outputStream.write(buffer, 0, len)
+                }
+            } while (len != -1)
+
+            return outputStream.toByteArray()
+        }
+
+        fun isGoogleLogListVerified(context: Context): Boolean {
+            val sigBytes = toByteArray(context.assets.open("log_list.sig"))
+            val publicKeyBytes = loadPEMFile(context.assets.open("log_list_pubkey.pem"))
+            val jsonBytes = toByteArray(context.assets.open("log_list.json"))
+            val keyFactory = KeyFactory.getInstance("RSA")
+            val signature = Signature.getInstance("SHA256withRSA").apply {
+                initVerify(keyFactory.generatePublic(X509EncodedKeySpec(publicKeyBytes)))
+                update(jsonBytes)
+            }
+            val isVerified = signature.verify(sigBytes)
+            Log.d(MainJavaActivity.TAG, "isSignatureVerified: $isVerified")
+            return isVerified
+        }
+    }
+
+    private lateinit var textView: TextView
+    private lateinit var uppercaseAlgorithm: Button
+    private lateinit var lowercaseAlgorithm: Button
+    private var isThreadRunning = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(id.text)
+        uppercaseAlgorithm = findViewById(id.uppercase)
+        lowercaseAlgorithm = findViewById(id.lowercase)
+
+        uppercaseAlgorithm.setOnClickListener(this)
+        lowercaseAlgorithm.setOnClickListener(this)
+
+        Thread {
+            isGoogleLogListVerified(applicationContext)
+        }.also {
+            it.start()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v === uppercaseAlgorithm) {
+            verifyLogList(MainJavaActivity.UPPERCASE_WITH_SHA256_WITH_RSA)
+        } else if (v === lowercaseAlgorithm) {
+            verifyLogList(MainJavaActivity.LOWERCASE_SHA256_WITH_RSA)
+        }
+    }
+
+    private fun toggleButtons(enabled: Boolean) {
+        uppercaseAlgorithm.isEnabled = enabled
+        lowercaseAlgorithm.isEnabled = enabled
+    }
+
+    private fun onVerificationComplete(displayText: String) {
+        isThreadRunning = false
+        toggleButtons(true)
+        textView.text = displayText
+    }
+
+    private fun verifyLogList(algorithm: String) {
+        if (isThreadRunning) {
+            return
+        }
+        Thread {
+            try {
+                val result = isGoogleLogListVerified(applicationContext)
+                textView.post {
+                    onVerificationComplete(if (result) "$algorithm: Log List is Verified" else "$algorithm: Log list FAILED verification")
+                }
+            } catch (e: Exception) {
+                textView.post {
+                    onVerificationComplete(
+                        algorithm + ": Error while verifying log list.\n" + e.message
+                    )
+                }
+                e.printStackTrace()
+            }
+        }.also {
+            it.start()
+        }
+        toggleButtons(false)
+        isThreadRunning = true
+    }
+
+    // JAVA 8 Version that does the same thing
+//    public static void main(String[] args) throws IOException, InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, SignatureException {
+//        FileInputStream fis = new FileInputStream(INSERT_FILE_PATH_TO_log_list.sig);
+//        byte[] sigBytes = toByteArray(fis);
+////		System.out.println(sigBytes.length);
+//        byte[] pubBytes = loadPEM(INSERT_FILE_PATH_TO_log_list_pubkey.pem);
+//        byte[] json = toByteArray(new FileInputStream(INSERT_FILE_PATH_TO_log_list.json));
+//        KeyFactory kf = KeyFactory.getInstance("RSA");
+//        Signature sig = Signature.getInstance("SHA256withRSA");
+//        System.out.println(kf.generatePublic(new X509EncodedKeySpec(pubBytes)));
+//        sig.initVerify(kf.generatePublic(new X509EncodedKeySpec(pubBytes)));
+//        sig.update(json);
+//        System.out.println(sig.verify(sigBytes));
+//    }
+//    public static byte[] toByteArray(InputStream in) throws IOException {
+//        ByteArrayOutputStream os = new ByteArrayOutputStream();
+//        byte[] buffer = new byte[1024];
+//        int len;
+//        // read bytes from the input stream and store them in buffer
+//        while ((len = in.read(buffer)) != -1) {
+//            // write bytes from the buffer into output stream
+//            os.write(buffer, 0, len);
+//        }
+//        return os.toByteArray();
+//    }
+//    private static byte[] loadPEM(String resource) throws IOException {
+//        byte[] pubBytes = toByteArray(new FileInputStream(resource));
+//        String pem = new String(pubBytes);
+//        Pattern parse = Pattern.compile("(?m)(?s)^---*BEGIN.*---*$(.*)^---*END.*---*$.*");
+//        String encoded = parse.matcher(pem).replaceFirst("$1");
+//        return Base64.getMimeDecoder().decode(encoded);
+//    }
+}
---
+++ app/src/main/java/com/example/signaturetestapp/MainJavaActivity.java
@@ -0,0 +1,158 @@
+package com.example.signaturetestapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.regex.Pattern;
+
+public class MainJavaActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = "GoogleCertificateTransparencyLogListVerifier";
+
+    public static final String UPPERCASE_WITH_SHA256_WITH_RSA = "SHA256WithRSA";
+
+    public static final String LOWERCASE_SHA256_WITH_RSA = "SHA256withRSA";
+
+    private TextView mTextView;
+
+    private Button mUppercaseAlgorithm, mLowercaseAlgorithm;
+
+    private boolean mIsThreadRunning = false;
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.text);
+        mUppercaseAlgorithm = findViewById(R.id.uppercase);
+        mLowercaseAlgorithm = findViewById(R.id.lowercase);
+
+        mUppercaseAlgorithm.setOnClickListener(this);
+        mLowercaseAlgorithm.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(final View v) {
+        if (v == mUppercaseAlgorithm) {
+            verifyLogList(UPPERCASE_WITH_SHA256_WITH_RSA);
+        } else if (v == mLowercaseAlgorithm) {
+            verifyLogList(LOWERCASE_SHA256_WITH_RSA);
+        }
+    }
+
+    private void toggleButtons(boolean enabled) {
+        mUppercaseAlgorithm.setEnabled(enabled);
+        mLowercaseAlgorithm.setEnabled(enabled);
+    }
+
+    private void verifyLogList(final String algorithm) {
+        if (mIsThreadRunning) {
+            return;
+        }
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    final boolean result = isGoogleLogListVerified(MainJavaActivity.this, algorithm);
+                    mTextView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            onVerificationComplete(result ? algorithm + ": Log List is Verified"
+                                    : algorithm + ": Log list FAILED verification");
+                        }
+                    });
+                } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException | SignatureException e) {
+                    mTextView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            onVerificationComplete(
+                                    algorithm + ": Error while verifying log list.\n" + e.getMessage());
+                        }
+                    });
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+        toggleButtons(false);
+        mIsThreadRunning = true;
+    }
+
+    private void onVerificationComplete(final String displayText) {
+        mIsThreadRunning = false;
+        toggleButtons(true);
+        mTextView.setText(displayText);
+    }
+
+    public static boolean isGoogleLogListVerified(Context context, final String algorithm)
+            throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException,
+            SignatureException {
+        byte[] sigBytes = toByteArray(context.getAssets().open("log_list.sig"));
+
+        byte[] pubBytes = loadPEM(context.getAssets().open("log_list_pubkey.pem"));
+        byte[] json = toByteArray(context.getAssets().open("log_list.json"));
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        Signature sig = Signature.getInstance(algorithm);
+        Log.d(TAG, "" + kf.generatePublic(new X509EncodedKeySpec(pubBytes)));
+        sig.initVerify(kf.generatePublic(new X509EncodedKeySpec(pubBytes)));
+        sig.update(json);
+        boolean verified = sig.verify(sigBytes);
+        Log.d(TAG, "" + verified);
+        return verified;
+    }
+
+    public static byte[] toByteArray(InputStream in) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len;
+        // read bytes from the input stream and store them in buffer
+        while ((len = in.read(buffer)) != -1) {
+            // write bytes from the buffer into output stream
+            os.write(buffer, 0, len);
+        }
+        return os.toByteArray();
+    }
+
+    private static byte[] loadPEM(InputStream inputStream) throws IOException {
+        byte[] pubBytes = toByteArray(inputStream);
+        String pem = new String(pubBytes);
+        Pattern parse = Pattern.compile("(?m)(?s)^---*BEGIN.*---*$(.*)^---*END.*---*$.*");
+        String encoded = parse.matcher(pem).replaceFirst("$1");
+        return Base64.getMimeDecoder().decode(encoded);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull final MenuItem item) {
+        if (item.getItemId() == R.id.kotlin) {
+            startActivity(new Intent(this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
